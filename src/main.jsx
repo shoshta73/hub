50b86af27e855d82871e5e4e5da25ae28916e422
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2025-01-06 03:55:16 +0100

    chore: added react and react tooling
    - added react and react-dom as dependencies
    - added react and react-dom types for type support
    - added vite config

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import App from "./App.jsx";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+let root = document.getElementById("app");
+if (root === null) {
+  root = document.createElement("div");
+  root.id = "app";
+  document.body.appendChild(root);
+}
+
+const rootElement = createRoot(root);
+rootElement.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
