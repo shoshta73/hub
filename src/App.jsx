ace0bc58cfdf32dfb680efec2deb9edb644c616d
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2025-01-06 19:43:44 +0100

    feat: Render todo list
    - Added typedef for todo object

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,51 @@
+/**
+ * @typedef {Object} Todo - Todo object
+ * @property {string} title - Todo title
+ * @property {number} status - Todo status (0: pending, 1: paused, 2: done)
+ * @property {string} id - Todo id
+ */
+
+/** @type {Todo[]} */
+const todos = [
+  {
+    title: "Buy milk",
+    status: 0,
+    id: "1",
+  },
+  {
+    title: "Buy bread",
+    status: 0,
+    id: "2",
+  },
+  {
+    title: "Buy eggs",
+    status: 0,
+    id: "3",
+  },
+];
+
+/**
+ * Renders list of todo items
+ *
+ * @param {{ items: Todo[]}} props - List of todo items
+ */
+function ItemsList({ items }) {
+  return (
+    <div>
+      {items.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
+    </div>
+  );
+}
+
 function App() {
-  return <h1>Hub</h1>;
+  return (
+    <div>
+      <h1>Todo list</h1>
+      <ItemsList items={todos} />
+    </div>
+  );
 }
 
 export default App;
